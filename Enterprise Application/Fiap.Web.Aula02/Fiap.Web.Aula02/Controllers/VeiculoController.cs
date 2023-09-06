24dc96a1e085529c7bff60554be86ad238f909f0
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-06 20:54:48 -0300

    Aula 02 - Aluguel de Veículos

---
+++ Enterprise Application/Fiap.Web.Aula02/Fiap.Web.Aula02/Controllers/VeiculoController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Fiap.Web.Aula02.Models;
+
+namespace Fiap.Web.Aula02.Controllers
+{
+    public class VeiculoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Veiculo veiculo)
+        {
+            return View(veiculo);
+        }
+    }
+}
